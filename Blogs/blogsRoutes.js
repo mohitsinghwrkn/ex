8a82914491b2944f0b165a8526eaa066d03dfe25
mohitsinghwrkn <mohitsinghwrkn@gmail.com>
2023-07-31 11:37:08 +0530

    a

--- Blogs/blogsRoutes.js
+++ Blogs/blogsRoutes.js
@@ -1,9 +1,13 @@
 const express = require("express");
 const router = express.Router();
-const path = require("path")
+const path = require("path");
 
-router.get('/css', (req, res) => {
-    res.sendFile(path.join(__dirname, "../views/blogs/css.html"));
-})
-  
-module.exports = router
+router.get("/css", (req, res) => {
+  res.sendFile(path.join(__dirname, "../views/blogs/css.html"));
+});
+
+router.get("/html", (req, res) => {
+  res.sendFile(path.join(__dirname, "../views/html.html"));
+});
+
+module.exports = router;
