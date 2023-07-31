9f1f82b663664030ee74d424946009ff2cf776f3
mohitsinghwrkn <mohitsinghwrkn@gmail.com>
2023-07-31 11:53:24 +0530

    SLV

--- Blogs/blogsRoutes.js
+++ Blogs/blogsRoutes.js
@@ -3,11 +3,12 @@ const router = express.Router();
 const path = require("path");
 
 router.get("/css", (req, res) => {
-  res.sendFile(path.join(__dirname, "../views/blogs/CSS.html"));
+  res.sendFile(path.join(__dirname, "../views/Blogs/CSS.html"));
 });
 
-router.get("/html", (req, res) => {
-  res.sendFile(path.join(__dirname, "../views/Blogs/css.html"));
-});
+// THIS CODE BELOW IS HAVING WRONG PATH DUE TO THE CASE SENSITIVITY IN LINUX MAYBE 
+// router.get("/css", (req, res) => {
+//   res.sendFile(path.join(__dirname, "../views/Blogs/CSS.html"));
+// }); 
 
 module.exports = router;
