2572d70ccf38a64666d4c79d82cf534cc4d3700e
mohitsinghwrkn <mohitsinghwrkn@gmail.com>
2023-07-31 11:10:54 +0530

    bld

--- Blogs/blogsRoutes.js
+++ Blogs/blogsRoutes.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const path = require("path")
 
 router.get('/css', (req, res) => {
-    res.sendFile(path.join(__dirname, "../views/blogs/z.html"));
+    res.sendFile(path.join(__dirname, "../views/blogs/css.html"));
 })
   
 module.exports = router
